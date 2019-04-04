1e5c288ddf17ed5728a0f241609530906fbae3d6
Gersen James <gersen.jorge@gmail.com>
2019-04-04 11:56:20 -0300

    Criando o módulo jogo-da-velha

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        JogoDaVelhaModule,
       ],
     }).compileComponents();
   }));
